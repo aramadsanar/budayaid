39a8ea12990d1e5e0a98fb08e2381b81f06eb649
Armada Nasar <armadanasar@gmail.com>
2018-05-09 12:32:58 +0700

    Created early adapter for budaya items that are going to be used in all categories.

--- budayaid_server/vagrant/install.sh
+++ budayaid_server/vagrant/install.sh
@@ -5,12 +5,10 @@ sudo apt-get install -y python3-pip unzip
 sudo -H pip3 install sqlalchemy flask
 mkdir budayaid_srv
 sudo chown armadanasar budayaid_srv
-unzip budayaid_install_package.zip -d ./budayaid_srv
+unzip budayaid_backend_install_package.zip -d ./budayaid_srv
 cd budayaid_srv
 mkdir imagepool
 sudo chown armadanasar *
-python3 budayaid_database_config.py
-python3 budayaid_add_dummy_province.py
 python3 budayaid_srv_app.py
 cd ..
 rm install.sh
